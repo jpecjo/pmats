04f8c1ecf4a0752450d8d5b82ef06ff7c44b0168
Rod Pecjo <jpecjo@gmail.com>
2017-06-23 15:49:47 +0800

    Investigating dates

--- modules/candidates/CandidatesUI.php
+++ modules/candidates/CandidatesUI.php
@@ -856,7 +856,6 @@ class CandidatesUI extends UserInterface
                 'middleName'      => $this->getTrimmedInput('middleName', $_POST),
                 'lastName'        => $this->getTrimmedInput('lastName', $_POST),
                 'alias'           => $this->getTrimmedInput('alias', $_POST),
-                // 'dob'             => $this->getTrimmedInput('dob', $_POST),
                 'email1'          => $this->getTrimmedInput('email1', $_POST),
                 'email2'          => $this->getTrimmedInput('email2', $_POST),
                 'phoneHome'       => $this->getTrimmedInput('phoneHome', $_POST),
@@ -1279,7 +1278,6 @@ class CandidatesUI extends UserInterface
         $middleName      = $this->getTrimmedInput('middleName', $_POST);
         $lastName        = $this->getTrimmedInput('lastName', $_POST);
         $alias           = $this->getTrimmedInput('alias', $_POST);
-        // $dob             = $this->getTrimmedInput('dob', $_POST);
         $email1          = $this->getTrimmedInput('email1', $_POST);
         $email2          = $this->getTrimmedInput('email2', $_POST);
         $address         = $this->getTrimmedInput('address', $_POST);
