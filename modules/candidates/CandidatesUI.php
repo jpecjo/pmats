1bbfe10db33026e6f20c41caa0c5cf9ff461c731
Rod Pecjo <jpecjo@gmail.com>
2017-06-23 14:22:13 +0800

    Again 2

--- modules/candidates/CandidatesUI.php
+++ modules/candidates/CandidatesUI.php
@@ -1279,7 +1279,7 @@ class CandidatesUI extends UserInterface
         $middleName      = $this->getTrimmedInput('middleName', $_POST);
         $lastName        = $this->getTrimmedInput('lastName', $_POST);
         $alias           = $this->getTrimmedInput('alias', $_POST);
-        $dob             = $this->getTrimmedInput('dob', $_POST);
+        // $dob             = $this->getTrimmedInput('dob', $_POST);
         $email1          = $this->getTrimmedInput('email1', $_POST);
         $email2          = $this->getTrimmedInput('email2', $_POST);
         $address         = $this->getTrimmedInput('address', $_POST);
